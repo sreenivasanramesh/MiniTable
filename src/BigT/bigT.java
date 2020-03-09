c78806bc4766aa14cdd2722b105a2b146cc946da
Rakesh R <rakesh2693@gmail.com>
2020-03-09 16:04:54 -0700

    Updating Map and adding method to file index names

--- src/BigT/bigT.java
+++ src/BigT/bigT.java
@@ -2,13 +2,53 @@ package BigT;
 
 import global.MID;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class bigT {
 
     int type;
+    String name;
+    String indexNames[];
+
 
     // Initialize the big table.typeis an integer be-tween 1 and 5 and the different types will correspond to different clustering and indexing strategies youwill use for the bigtable.
-    void bigT(String name, int type) {
+    public void bigT(String name, int type) throws Exception {
         this.type = type;
+        this.name = name;
+        createIndex();
+    }
+
+    private void createIndex() throws Exception {
+        switch (this.type) {
+            case 1:
+                this.indexNames = new String[]{};
+                break;
+            case 2:
+                this.indexNames = new String[]{this.name + "_row.idx"};
+                break;
+            case 3:
+                this.indexNames = new String[]{this.name + "_column.idx"};
+                break;
+            case 4:
+                this.indexNames = new String[]{this.name + "_column_row.idx", this.name + "_timestamp.idx"};
+                break;
+            case 5:
+                this.indexNames = new String[]{this.name + "_row_value.idx", this.name + "_timestamp.idx"};
+                break;
+            default:
+                throw new Exception("Invalid Type Passed");
+        }
+        for (String indexName : this.indexNames) {
+            File file = new File(indexName);
+            if (!file.exists()) {
+                String[] tempArray = indexName.substring(0, indexName.lastIndexOf('.')).split("_");
+                String[] indexArray = Arrays.copyOfRange(tempArray, 1, tempArray.length);
+                for (String index : indexArray) {
+                    // Btree Index to create Index
+                }
+            }
+        }
     }
 
     //Delete the bigtable from the database.
