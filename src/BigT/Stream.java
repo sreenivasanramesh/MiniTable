e25e086df8354cbf928ca7c2cdad40c6aa778258
ganesh <ganeshyallankar@gmail.com>
2020-03-10 17:42:42 -0700

    adding type and ordertype in Stream

--- src/BigT/Stream.java
+++ src/BigT/Stream.java
@@ -1,104 +1,63 @@
 package BigT;
 
-import diskmgr.Page;
-import global.*;
-
+import global.MID;
 import java.io.IOException;
-import heap.*;
-import bufmgr.*;
+import heap.Scan;
+import heap.Heapfile;
+import heap.InvalidTupleSizeException;
+
 
 public class Stream extends Scan  {
 
-    /*
-    Initialize a stream of maps on bigtable
-    */
+    /** Initialize a stream of maps on big-table */
     Heapfile heapfile;
     String rowFilter;
     String columnFilter;
     String valueFilter;
+    bigT bigTable;
+    int orderType;
+    int type;
+    String starFilter = new String("*");
+    String rangeRegex = new String("\\[\\d+, \\d+\\]");
 
 
     /* Pending */
-    public Stream(bigT bigtable, int orderType, String rowFilter, String columnFilter, String valueFilter) throws InvalidTupleSizeException, IOException {
-        /* constructor */
+    public Stream(bigT bigtable, int orderType, String rowFilter, String columnFilter, String valueFilter, int type) throws InvalidTupleSizeException, IOException {
         super(bigtable.heapfile);
 
         int mapCount = bigtable.getMapCnt();
         int rowCount = bigtable.getRowCnt();
         int columnCount = bigtable.getColumnCnt();
+        this.bigTable = bigtable;
+        this.orderType = orderType;
         this.heapfile = bigtable.heapfile;
         this.rowFilter = rowFilter;
         this.columnFilter = columnFilter;
         this.valueFilter = valueFilter;
-        String star_filter = new String("*");
-        Heapfile heapfile = bigtable.heapfile;
-        String rangeRegex = new String("\\[\\d+, \\d+\\]");
-        if (rowFilter.matches(rangeRegex)) {
+        this.type = type;
 
-        }
-        else {
-            // should match single element.
-        }
-
-        }
-
-        if (star_filter.equals(rowFilter)) {
-            if (star_filter.equals(columnFilter)) {
-                if(star_filter.equals(valueFilter)) {
-                    // row, col, val = *, * , * - so sequential scan on all elements
-                    //no changes required, scan already pointing to first mid.
-                }
-
-            }
+        /** Based on type decide index
+         * check if index exists for bigtable
+         * if exists then query based on query conditions
+         * else sequential scan.
+         */
+        
     }
 
 
+    /**
+     *  Closes open stream. Same as Heap.Scan.closeScan.
+     */
     public void closeStream() {
         super.closescan();
     }
 
-    /*
+
+    /**
     Retrieve the next map in the stream
     */
     public Map getNext(MID mid) throws Exception {
         return super.getNext(mid);
     }
-
-    public Tuple getNext(RID rid)
-            throws InvalidTupleSizeException,
-            IOException {
-        Tuple recptrtuple = null;
-
-        boolean nextUserStatus = super.getNextUserStatus();
-        HFPage datapage = super.getDataPage();
-        RID userrid = super.getUserId();
-
-        if (nextUserStatus != true) {
-            super.nextDataPage();
-        }
-
-        if (datapage == null)
-            return null;
-
-        rid.pageNo.pid = userrid.pageNo.pid;
-        rid.slotNo = userrid.slotNo;
-
-        try {
-            recptrtuple = datapage.getRecord(rid);
-        } catch (Exception e) {
-            //System.err.println("SCAN: Error in Scan" + e);
-            e.printStackTrace();
-        }
-
-        super.setUserId(datapage.nextRecord(rid));
-        if (userrid == null) super.setNextUserStatus(false);
-        else super.setNextUserStatus(true);
-
-        return recptrtuple;
-    }
-
-
-
-
 
 }
