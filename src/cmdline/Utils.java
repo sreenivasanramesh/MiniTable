d199a9c4c52f93329b3c58b83556dbda767477fc
Sreenivasan Ramesh <vasan@MacbookPro14.local>
2020-03-18 01:10:07 -0700

    conditional expression for querying

--- src/cmdline/Utils.java
+++ src/cmdline/Utils.java
@@ -5,16 +5,19 @@ import global.SystemDefs;
 import heap.Heapfile;
 import heap.Tuple;
 import bufmgr.*;
-import global.AttrType;
+import global.*;
 import BigT.*;
 import diskmgr.pcounter;
+import iterator.CondExpr;
+import iterator.FldSpec;
+import iterator.RelSpec;
 
 import java.io.*;
 import static global.GlobalConst.NUMBUF;
 
 class Utils {
 
-    private static final int NUM_PAGES = 700000;
+    private static final int NUM_PAGES = 10000;
 
     static void batchInsert(String dataFile, String tableName, int type) throws IOException, PageUnpinnedException, PagePinnedException, PageNotFoundException, BufMgrException, HashOperationException
     {
@@ -75,7 +78,52 @@ class Utils {
         SystemDefs.JavabaseDB.closeDB();
         System.out.println("Reads:  " + pcounter.rcounter);
         System.out.println("Writes: " + pcounter.wcounter);
+    }
+
+    static void query(String tableName, Integer type, Integer orderType, String rowFilter, String colFilter, String valFilter, Integer NUMBUF) {
 
 
     }
+
+    static CondExpr[] getCondExpr(String filter){
+        if (filter.equals("*")){
+            return null;
+        }
+        else if (filter.contains(",")){
+            String[] range = filter.replaceAll("[\\[ \\]]", "").split(",");
+            //cond expr of size 3 for range searches
+            CondExpr[] expr = new CondExpr[3];
+            expr[0] = new CondExpr();
+            expr[0].op = new AttrOperator(AttrOperator.aopGE);
+            expr[0].type1 = new AttrType(AttrType.attrSymbol);
+            expr[0].type2 = new AttrType(AttrType.attrString);
+            expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[0].operand2.string = range[0];
+            expr[0].next = null;
+            expr[1] = new CondExpr();
+            expr[1].op = new AttrOperator(AttrOperator.aopLE);
+            expr[1].type1 = new AttrType(AttrType.attrSymbol);
+            expr[1].type2 = new AttrType(AttrType.attrString);
+            expr[1].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[1].operand2.string = range[1];
+            expr[1].next = null;
+            expr[2] = null;
+            return expr;
+        }
+        else{
+            //equality search
+            CondExpr[] expr = new CondExpr[2];
+            expr[0] = new CondExpr();
+            expr[0].op = new AttrOperator(AttrOperator.aopEQ);
+            expr[0].type1 = new AttrType(AttrType.attrSymbol);
+            expr[0].type2 = new AttrType(AttrType.attrString);
+            expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[0].operand2.string = filter;
+            expr[0].next = null;
+            expr[1] = null;
+            return expr;
+        }
+    }
 }
+
+
