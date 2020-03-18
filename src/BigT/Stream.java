e8f29bf408dbc616b45adfa79a152f6c61872295
Sreenivasan Ramesh <vasan@MacbookPro14.local>
2020-03-18 01:13:50 -0700

    converting filters to cond expressions

--- src/BigT/Stream.java
+++ src/BigT/Stream.java
@@ -309,4 +309,45 @@ public class Stream extends Scan {
             System.err.println("------------------- index scan completed ---------------------\n");
         }while (tempObj != null);
     }
+
+
+    private CondExpr[] getCondExpr(String filter){
+        if (filter.equals("*")){
+            return null;
+        }
+        else if (filter.contains(",")){
+            String[] range = filter.replaceAll("[\\[ \\]]", "").split(",");
+            //cond expr of size 3 for range searches
+            CondExpr[] expr = new CondExpr[3];
+            expr[0] = new CondExpr();
+            expr[0].op = new AttrOperator(AttrOperator.aopGE);
+            expr[0].type1 = new AttrType(AttrType.attrSymbol);
+            expr[0].type2 = new AttrType(AttrType.attrString);
+            expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[0].operand2.string = range[0];
+            expr[0].next = null;
+            expr[1] = new CondExpr();
+            expr[1].op = new AttrOperator(AttrOperator.aopLE);
+            expr[1].type1 = new AttrType(AttrType.attrSymbol);
+            expr[1].type2 = new AttrType(AttrType.attrString);
+            expr[1].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[1].operand2.string = range[1];
+            expr[1].next = null;
+            expr[2] = null;
+            return expr;
+        }
+        else{
+            //equality search
+            CondExpr[] expr = new CondExpr[2];
+            expr[0] = new CondExpr();
+            expr[0].op = new AttrOperator(AttrOperator.aopEQ);
+            expr[0].type1 = new AttrType(AttrType.attrSymbol);
+            expr[0].type2 = new AttrType(AttrType.attrString);
+            expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[0].operand2.string = filter;
+            expr[0].next = null;
+            expr[1] = null;
+            return expr;
+        }
+    }
 }
