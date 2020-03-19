647a2c8e3d3123c724ab0b82fbbe2609c8a666bd
Sreenivasan Ramesh <vasan@MacbookPro14.local>
2020-03-19 00:57:09 -0700

    Stream2

---
+++ src/BigT/Stream2.java
@@ -0,0 +1,108 @@
+package BigT;
+
+import btree.BTreeFile;
+import btree.IndexFileScan;
+import bufmgr.PageNotReadException;
+import global.AttrOperator;
+import global.AttrType;
+import global.MID;
+import heap.Heapfile;
+import heap.InvalidTupleSizeException;
+import heap.InvalidTypeException;
+import heap.Tuple;
+import index.IndexException;
+import iterator.*;
+
+import java.io.IOException;
+
+public class Stream2 extends Iterator {
+
+    /**
+     * Initialize a stream of maps on big-table
+     */
+    Heapfile heapFile;
+    BTreeFile indexFile;
+    CondExpr[] rowFilter;
+    CondExpr[] columnFilter;
+    CondExpr[] valueFilter;
+    bigT bigTable;
+    int orderType;
+    int type;
+    MID mid = null;
+    String starFilter = new String("*");
+    String rangeRegex = new String("\\[\\d+, \\d+\\]");
+    private IndexFileScan btIndScan;
+
+
+    /* Pending */
+    public Stream2(bigT bigTable, int orderType, String rowFilter, String columnFilter, String valueFilter, int type) throws Exception {
+
+        //scan - healfile? init?
+        this.bigTable = bigTable;
+        this.type = type;
+        this.orderType = orderType;
+
+        //based on type either heapFile or indexFile will be selected
+        this.heapFile = bigTable.heapfile;
+        this.indexFile = bigTable.indexFile;
+        this.rowFilter = getCondExpr(rowFilter);
+        this.columnFilter = getCondExpr(columnFilter);
+        this.valueFilter = getCondExpr(valueFilter);
+
+        //do something and create an iterator object
+
+    }
+
+    //change to return Map
+    @Override
+    public Tuple get_next() throws IOException, JoinsException, IndexException, InvalidTupleSizeException, InvalidTypeException, PageNotReadException, TupleUtilsException, PredEvalException, SortException, LowMemException, UnknowAttrType, UnknownKeyTypeException, Exception {
+        return null;
+    }
+
+    //change to
+    @Override
+    public void close() throws IOException, JoinsException, SortException, IndexException {
+
+    }
+
+
+    private CondExpr[] getCondExpr(String filter){
+        if (filter.equals("*")){
+            return null;
+        }
+        else if (filter.contains(",")){
+            String[] range = filter.replaceAll("[\\[ \\]]", "").split(",");
+            //cond expr of size 3 for range searches
+            CondExpr[] expr = new CondExpr[3];
+            expr[0] = new CondExpr();
+            expr[0].op = new AttrOperator(AttrOperator.aopGE);
+            expr[0].type1 = new AttrType(AttrType.attrSymbol);
+            expr[0].type2 = new AttrType(AttrType.attrString);
+            expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[0].operand2.string = range[0];
+            expr[0].next = null;
+            expr[1] = new CondExpr();
+            expr[1].op = new AttrOperator(AttrOperator.aopLE);
+            expr[1].type1 = new AttrType(AttrType.attrSymbol);
+            expr[1].type2 = new AttrType(AttrType.attrString);
+            expr[1].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[1].operand2.string = range[1];
+            expr[1].next = null;
+            expr[2] = null;
+            return expr;
+        }
+        else{
+            //equality search
+            CondExpr[] expr = new CondExpr[2];
+            expr[0] = new CondExpr();
+            expr[0].op = new AttrOperator(AttrOperator.aopEQ);
+            expr[0].type1 = new AttrType(AttrType.attrSymbol);
+            expr[0].type2 = new AttrType(AttrType.attrString);
+            expr[0].operand1.symbol = new FldSpec(new RelSpec(RelSpec.outer), 2);
+            expr[0].operand2.string = filter;
+            expr[0].next = null;
+            expr[1] = null;
+            return expr;
+        }
+    }
+}
