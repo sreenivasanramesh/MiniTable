3803982c0b17953e4134051b11fd3b24f8e2f244
sreenivasanramesh <sreenivasan.ramesh@gmail.com>
2020-04-21 15:50:44 -0700

    RowSort changes

--- src/BigT/RowSort.java
+++ src/BigT/RowSort.java
@@ -1,7 +1,9 @@
 package BigT;
 
+import cmdline.MiniTable;
+import global.TupleOrder;
 import heap.Heapfile;
-import iterator.Sort;
+import iterator.*;
 
 public class RowSort {
 
@@ -10,8 +12,9 @@ public class RowSort {
     private String column;
     private int numBuffers;
     private Stream mapStream;
+    private bigT bigTable;
     private Heapfile heapfile;
-    private Sort sort;
+    private MapSort sortObj;
 
     public RowSort(String inTable, String outTable, String column, int numBuffers) throws Exception {
         this.inTable = inTable;
@@ -19,55 +22,99 @@ public class RowSort {
         this.column = column;
         this.numBuffers = numBuffers;
 
-        bigT bigTable = new bigT(this.outTable, true);
-        this.mapStream = bigTable.openStream(1, "*", "*", "*");
+        this.bigTable = new bigT(this.outTable, true);
         this.heapfile = new Heapfile("temp_sort_file");
-
-        Map map = mapStream.getNext();
-
-
-
+        tempHeapFile();
+        Map map = sortTempHeapFile();
+        this.mapStream = this.bigTable.openStream(9999, map.getRowLabel(), "*", "*");
 
     }
 
-    public void sort() throws Exception {
 
+    private void tempHeapFile() throws Exception {
 
+        Stream mapStream = this.bigTable.openStream(1, "*", "*", "*");
         Map map = mapStream.getNext();
-        String value;
-        String row = null;
 
+        String value = "";
+        String row = "";
 
-        while(map != null){
-
-            if(map.getColumnLabel().equalsIgnoreCase(this.column)){
-                value = map.getValue();
-            }
-            if(!map.getRowLabel().equalsIgnoreCase(row)){
+        while(map != null)
+        {
+            if(!map.getRowLabel().equals(row)){
                 if(value.isEmpty()){
                     value = "zzzzz";
                 }
                 Map tempMap = new Map();
-                map.se
-
+                tempMap.setRowLabel(row);
+                tempMap.setColumnLabel("temp_column");
+                tempMap.setValue(value);
+                tempMap.setTimeStamp(1);
                 this.heapfile.insertMap(tempMap.getMapByteArray());
-
+                row = map.getRowLabel(); //next
+                value = "";
             }
-
-            //do something
-
+            if(map.getColumnLabel().equals(this.column)){
+                value = map.getValue();
+            }
+            map = mapStream.getNext();
         }
-        map = mapStream.getNext();
+
+        mapStream.closeStream();
+
+
         Map tempMap = new Map();
-        //fill tempMap
+        tempMap.setRowLabel(row);
+        tempMap.setColumnLabel("temp_column");
+        tempMap.setValue(value);
+        tempMap.setTimeStamp(1);
         this.heapfile.insertMap(tempMap.getMapByteArray());
+    }
+
+    private Map sortTempHeapFile() throws Exception {
+        FldSpec[] projection = new FldSpec[4];
+        RelSpec rel = new RelSpec(RelSpec.outer);
+        projection[0] = new FldSpec(rel, 1);
+        projection[1] = new FldSpec(rel, 2);
+        projection[2] = new FldSpec(rel, 3);
+        projection[3] = new FldSpec(rel, 4);
+
+        FileScan fscan = null;
+        try {
+            fscan = new FileScan("temp_sort_file", MiniTable.BIGT_ATTR_TYPES, MiniTable.BIGT_STR_SIZES, (short) 4, 4, projection, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
+        try {
+            int num_pages = 10;
+            this.sortObj = new MapSort(MiniTable.BIGT_ATTR_TYPES, MiniTable.BIGT_STR_SIZES, fscan, 4, new TupleOrder(TupleOrder.Ascending), num_pages, MiniTable.BIGT_STR_SIZES[1], false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-
-
-
-
+        Map map = sortObj.get_next();
+        return map;
 
     }
+
+    public Map getNext() throws Exception {
+        Map map = this.mapStream.getNext();
+        if(map == null){
+            mapStream.closeStream();
+            Map NextVal = this.sortObj.get_next();
+            if (NextVal == null)
+                return null;
+            Stream tempMapStream = this.bigTable.openStream(9999, NextVal.getRowLabel(), "*", "*");
+            map = tempMapStream.getNext();
+            if(map == null)
+                tempMapStream.closeStream();
+        }
+
+        return map;
+    }
+
+
+
 }
